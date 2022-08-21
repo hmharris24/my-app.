3f5e474867e40653c230903da6530fbc2bbafa8c
hmharris24 <102563834+hmharris24@users.noreply.github.com>
2022-08-21 14:27:38 -0500

    Moved footer to App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,19 @@ export default function App() {
   return (
     <div className="App">
       <Weather />
+      <span className="footer">
+        This project was coded by Holly Meyers and is
+        <a href="https://github.com/hmharris24/my-app." target="blank">
+          open-sourced on GitHub
+        </a>
+        and
+        <a
+          href="https://app.netlify.com/sites/cheery-puffpuff-a66b1b/deploys"
+          target="blank"
+        >
+          hosted on Netlify
+        </a>
+      </span>
     </div>
   );
 }
