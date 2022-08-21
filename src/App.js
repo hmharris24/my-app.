7e9723eacb07227061ce4fe05d2dd9071722ba77
hmharris24 <102563834+hmharris24@users.noreply.github.com>
2022-08-21 14:48:06 -0500

    Updated footer

--- src/App.js
+++ src/App.js
@@ -12,16 +12,17 @@ export default function App() {
         <Weather defaultCity="Paris" />
       </div>
       <span className="footer">
-        This project was coded by Holly Meyers and is
+        This project was coded by Holly Meyers and is{" "}
         <a href="https://github.com/hmharris24/my-app." target="blank">
           open-sourced on GitHub
-        </a>
-        and
+        </a>{" "}
+        and{" "}
         <a
           href="https://app.netlify.com/sites/cheery-puffpuff-a66b1b/deploys"
           target="blank"
         >
-          hosted on Netlify
+          {" "}
+          hosted on Netlify.
         </a>
       </span>
     </div>
